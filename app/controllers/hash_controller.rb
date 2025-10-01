51308cf73d74f15f8f1c0d8bfbe47ce25388f6b1
Eloisa Lanzas <eloisalanzas24@gmail.com>
2025-09-30 18:37:54 -0600

    datos de la persona

--- app/controllers/hash_controller.rb
+++ app/controllers/hash_controller.rb
@@ -1,4 +1,10 @@
 class HashController < ApplicationController
   def mostrar
+    @persona = {
+      nombre: "Mario",
+      apellido: "Pérez",
+      telefono: "89221772",
+      correo: "mario@gmail.com"
+    }
   end
 end
